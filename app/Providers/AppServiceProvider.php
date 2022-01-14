c508626f629d1034b787722a7b474ef6df2c8646
NguyenXuanDuong0612 <you@example.com>
2022-01-14 22:25:47 +0700

    CRUD faculty

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            \App\Repositories\Faculties\FacultyRepositoryInterface::class,
+            \App\Repositories\Faculties\FacultyRepository::class
+        );
+      
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Mr. Dương</p>
+    <a href="">duonglimiter@gmail.com</a>
+</footer>
+</body>
+</html>
